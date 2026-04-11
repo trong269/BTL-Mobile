624faf2315a70d0351cf597103ebedd328ef88e8
nhatvan1912 <112192624+nhatvan1912@users.noreply.github.com>
2026-04-11 17:25:01 +0700

    [FEAT] read chapters + basicUI

---
+++ BookApp/app/src/main/java/com/bookapp/data/model/Chapter.kt
@@ -0,0 +1,9 @@
+package com.bookapp.data.model
+
+data class Chapter(
+    val id: String? = null,
+    val bookId: String? = null,
+    val chapterNumber: Int? = null,
+    val title: String? = null,
+    val content: String? = null
+)
